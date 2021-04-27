376b756335d6f4b6b2429a4718b348269cb2bf0b
Hasanuzzaman <shuvo009@yahoo.com>
2021-04-27 23:44:57 +0600

    Selection Sort

---
+++ AlgoDsc/SelectionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgoDsc
+{
+    public class SelectionSort
+    {
+        [Fact]
+        public void Selection_sort_test()
+        {
+            var result = SelectionSortFunc(new[] { 14, 33, 27, 10, 35, 19, 42, 44 });
+            Assert.Equal(new[] { 10, 14, 19, 27, 33, 35, 42, 44 }, result);
+        }
+
+
+        public int[] SelectionSortFunc(int[] input)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                var minIndex = i;
+             
+                for (var j = i + 1; j < input.Length; j++)
+                {
+                    if (input[j] < input[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                var temp = input[minIndex];
+                input[minIndex] = input[i];
+                input[i] = temp;
+            }
+
+            return input;
+        }
+    }
+}
