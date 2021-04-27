5b0ae46a4855749ae473a5fe145d271b320c9593
Hasanuzzaman <shuvo009@yahoo.com>
2021-04-27 21:55:25 +0600

    Char Sort

---
+++ AlgoDsc/CharSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgoDsc
+{
+    public class CharSort
+    {
+        [Fact]
+        public void Chart_sort_test()
+        {
+            var result = CharSortFunc("acebfd");
+            Assert.Equal("abcdef", result);
+        }
+
+
+        public string CharSortFunc(string input)
+        {
+            var charList = input.ToCharArray();
+
+            for (int i = 0; i < charList.Length - 1; i++)
+            {
+                for (int j = i + 1; j < charList.Length; j++)
+                {
+                    if (charList[i] > charList[j])
+                    {
+                        var temp = charList[i];
+                        charList[i] = charList[j];
+                        charList[j] = temp;
+                    }
+                }
+            }
+
+            return new string(charList);
+        }
+    }
+}
