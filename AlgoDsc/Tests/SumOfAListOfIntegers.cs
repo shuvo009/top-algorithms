9abe756482e0c6c1ca695e3b5629a42e38b4e21b
Hasanuzzaman <shuvo009@yahoo.com>
2021-05-08 10:06:44 +0600

    sum of a list of integers

---
+++ AlgoDsc/Tests/SumOfAListOfIntegers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgoDsc.Tests
+{
+    //Write a program to calculate the sum of a list of integers,
+    //the partial integer sum could overflow, but the sum is guaranteed to be able to fit into a 32bit int type.
+    public class SumOfAListOfIntegers
+    {
+        [Fact]
+        public void Sum()
+        {
+            var input = new int[] { 5, int.MaxValue, -500 };
+            var result = SumFunc(input);
+            Assert.Equal(2147483152, result);
+        }
+
+        private int SumFunc(int[] inputs)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                unchecked
+                {
+                    sum += inputs[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
