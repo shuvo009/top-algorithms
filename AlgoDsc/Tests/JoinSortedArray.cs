b7484d239493dc6b9502015418901075036daf8b
Hasanuzzaman <shuvo009@yahoo.com>
2021-05-08 09:33:35 +0600

    SumContiguousSubArray

---
+++ AlgoDsc/Tests/JoinSortedArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgoDsc.Tests
+{
+    public class JoinSortedArray
+    {
+        [Fact]
+        public void Join_sorted_array_test()
+        {
+            var result = JoinSortedArrayFunc(new[]{1,3,4,6}, new[]{1,2,3,5,7});
+            Assert.Equal(new[] {1, 2, 3, 4, 5, 6, 7}, result);
+        }
+
+        private int[] JoinSortedArrayFunc(int[] arrayOne, int[] arrayTwo)
+        {
+            var list = new List<int>();
+            list.AddRange(arrayOne.ToList());
+            list.AddRange(arrayTwo.ToList());
+
+            return list.Distinct().OrderBy(x => x).ToArray();
+        }
+    }
+}
---
+++ AlgoDsc/Tests/SumContiguousSubArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgoDsc.Tests
+{
+    public class SumContiguousSubArray
+    {
+        [Fact]
+        public void Sum_contiguous_sub_array()
+        {
+            var input = new int[] { -2, -3, 4, -1, -2, 1, 5, -3 };
+            var result = GetMaxSumContiguousSubArray(input);
+            Assert.Equal(7, result);
+        }
+
+        private int GetMaxSumContiguousSubArray(int[] input)
+        {
+            var maxSoFar = 0;
+            var maxEndingHere = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var val = input[i];
+                maxEndingHere += val;
+                
+                if (maxEndingHere < 0)
+                    maxEndingHere = 0;
+
+                if (maxEndingHere > maxSoFar)
+                    maxSoFar = maxEndingHere;
+            }
+
+            return maxSoFar;
+        }
+    }
+}
