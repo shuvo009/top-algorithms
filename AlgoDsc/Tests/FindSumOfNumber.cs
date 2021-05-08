426b4618050f62e43f5d58b9d8871276859eeeab
Hasanuzzaman <shuvo009@yahoo.com>
2021-05-08 15:06:12 +0600

    Find the numbers in the loop that the summation of which equal to some number

---
+++ AlgoDsc/Tests/FindSumOfNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgoDsc.Tests
+{
+    //Find the numbers in the loop that the summation of which equal to some number
+    public class FindSumOfNumber
+    {
+        [Fact]
+        public void Find_sum_of_number()
+        {
+            var input = new[] { 1, 4, 45, 6, 10, -8 };
+            var result = FindSumOfNumberFunc(input, 16);
+            Assert.True(result);
+        }
+
+        private bool FindSumOfNumberFunc(int[] input, int sum)
+        {
+            Array.Sort(input);
+
+            var start = 0;
+            var end = input.Length-1;
+
+            while (start!=end)
+            {
+                if (input[start] + input[end] > sum)
+                {
+                    end--;
+                }
+                else if (input[start] + input[end] < sum)
+                {
+                    start++;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
